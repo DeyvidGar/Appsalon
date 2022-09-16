dd17c58a4cd789a4710b9955c4b768855599903d
David G <davidgar8879@gmail.com>
2022-09-16 15:22:15 -0500

    cambios en el routing:bug

--- public/index.php
+++ public/index.php
@@ -91,3 +91,45 @@ $router->comprobarRutas();
 //     "require": {}
 // }
 // despues de hacerle cambios al archivo corremos el comando: composer update
+
+//PARA LE DEPLOYMENT NOTAS;
+// para el deployment debemos de hacer algunas configuraciones como el archivo .env que son variables de entorno para no mostrar el nombre del host usuario bd y pass
+//Dependencia para leer las varialbes de entorno: composer require vlucas/phpdotenv
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->safeload();
+//PARA EL DEPLOYMENT DEBEMOS OCULTAR ESTA INF EN UN ARCHIVO .ENV QUE CONTIENE VARIBLES DE ENTORNO QUE SERAN DE FORMA PRIVADA
+// $db = mysqli_connect(
+//     $_ENV['DB_HOST'], 
+//     $_ENV['DB_USER'], 
+//     $_ENV['DB_PASS'], 
+//     $_ENV['DB_BD'] 
+// );
+
+//para que el servidor se diriga a el archibo public y de ahi abra la app creamos el archivo .htacccess
+
+// en .gitignore colocamos el nombre de los archivos y carpetas que no deseamos subir a produccion como lo son herramientas de diseno node etc.
+
+//para subir el repo a gir desde git emplear los comandos que git nos proporcina
+// git init 
+// git add . (para todos los archivos)
+// git commit -m "Primer deployment"
+// git branch -M main (el branch main es la rama pricipal ojo con esta rama leer documentacion para mas detalles
+// git remote add origin https://github.com/DeyvidGar/Appsalon.git
+// git push -u origin main (para subirlo al remote anterior. Aqui podemos hacer push al servicio de hosting para subir a produccion los cambios)
+
+//para la base de datos
+// heroku config | grep CLEARDB_DATABASE_URL
+// CLEARDB_DATABASE_URL: mysql://b09728cd453532:bd860c9d@us-cdbr-east-06.cleardb.net/heroku_3e94bb146af57a1?reconnect=true
+
+//usuario
+// b09728cd453532
+//pass
+// bd860c9d
+//host
+// us-cdbr-east-06.cleardb.net
+//bd
+// heroku_3e94bb146af57a1
+
+//localmente tenemos asignado en nuestras variables de entorno nuestra conneccion local para cambiar esos valores en producion aplicacmos los sigientes comandos
+//PARA EL HOST
+// heroku config:set DB_HOST=us-cdbr-east-06.cleardb.net
